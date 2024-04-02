568823e520b729845ccc152a9395d0bcd2606e4e
jen-daum <jennifer.daum.me@gmail.com>
2024-04-02 15:27:21 +0100

    js - linking user input to API call

--- src/script.js
+++ src/script.js
@@ -2,27 +2,33 @@ function waitingInterpretation() {
   new Typewriter("#interpretation", {
     strings: `Finding meanings to your dream...`,
     autoStart: true,
+    delay: 30,
   });
   interpretDream();
 }
 
 function interpretDream() {
   let key = "ot351f8facfc3d0f699be8054cb84374";
-  let prompt = "cat with a hat";
+  let prompt = document.getElementById("dreamInput").value; // Retrieve user input
   let context =
-    "you are a dream interpreter expert. Provide a concise interpretation of the prompt as best as you can even if the user gave one keyword. The format should be one or two emotes followed by a few keywords relating to the meaning then an underscore followed by the meaning";
+    "you are a dream interpreter expert. Provide a concise interpretation of the prompt as best as you can even if the user gave one keyword. The format should be one or two emotes followed by a few keywords relating to the meaning then only one underscore here followed by the meaning which needs to be at least 30 words";
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
 
   console.log(apiUrl);
-  axios.get(apiUrl).then((response) => showAnswer(prompt, response)); //callback
+  axios.get(apiUrl).then((response) => showAnswer(prompt, response));
   console.log("dream meaning found");
 }
 
 function showAnswer(prompt, response) {
   console.log(response.data.answer);
+  // Split the answer on the first underscore and insert <br> tag
+  let [title, text] = response.data.answer.split("_");
+  let formattedAnswer = `<b>${prompt}</b> - ${title}<br/><br/><i>${text}</i>`;
+
   new Typewriter("#interpretation", {
-    strings: `${prompt} - ${response.data.answer}`,
+    strings: formattedAnswer,
     autoStart: true,
+    delay: 30,
   });
 }
 
